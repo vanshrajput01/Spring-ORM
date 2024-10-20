11b634c378553fb987a4c87291a8a95d7db3c2e5
Vansh Rajput <89916310+vanshrajput01@users.noreply.github.com>
2024-10-20 09:36:59 +0530

    hoe to save Multiple objects in database

--- MainPackage/App.java
+++ MainPackage/App.java
@@ -12,10 +12,34 @@ public class App
         ############################################################### create object of Entity class  ##########################################################
 
         Entity entity = new Entity();
-        entity.setEmpId(103);
         entity.setEmpName("emp03");
         entity.setEmpSalary(14000);
+        //                            save data
         System.out.println(employeeDeo.insertData(entity));
+
+
+        ##########################################################  to multiple objects at time in database #############################################
+         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        EmployeeDeo employeeDeo =  context.getBean("employeeDeo",EmployeeDeo.class);
+        // create object of Entity class
+        Entity entity = new Entity();
+        entity.setEmpName("emp04");
+        entity.setEmpSalary(14000);
+        
+        Entity entity01 = new Entity();
+        entity01.setEmpName("emp05");
+        entity01.setEmpSalary(15000);
+        
+        Entity entity02 = new Entity();
+        entity02.setEmpName("emp05");
+        entity02.setEmpSalary(15000);
+        
+        List entitiesList = List.of(entity,entity01,entity02);
+        
+//                                     save multiple object
+        System.out.println(employeeDeo.insertMultipleData(entitiesList) + "These ids data add in database..");
+
+        
 
         
     }
